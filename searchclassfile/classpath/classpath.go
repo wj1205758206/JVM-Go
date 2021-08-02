0a357f0997e6d635d8c77ac32a505bcca6fe8047
weijian <1205758206@qq.com>
2021-08-02 13:27:47 +0800

    实现Classpath结构体并测试搜索指定路径下的Class文件

---
+++ searchclassfile/classpath/classpath.go
@@ -0,0 +1,69 @@
+package classpath
+
+import (
+	"os"
+	"path/filepath"
+)
+
+type Classpath struct {
+	bootClasspath Entry
+	extClasspath  Entry
+	userClasspath Entry
+}
+
+func Parse(jreOption, cpOption string) *Classpath {
+	cp := &Classpath{}
+	cp.parseBootAndExtClasspath(jreOption)
+	cp.parseUserClasspath(cpOption)
+	return cp
+}
+func (self *Classpath) ReadClass(className string) ([]byte, Entry, error) {
+	className = className + ".class"
+	if data, entry, err := self.bootClasspath.readClass(className); err == nil {
+		return data, entry, err
+	}
+	if data, entry, err := self.extClasspath.readClass(className); err == nil {
+		return data, entry, err
+	}
+	return self.userClasspath.readClass(className)
+}
+func (self *Classpath) toString() string {
+	return self.userClasspath.toString()
+}
+
+func (self *Classpath) parseBootAndExtClasspath(jreOption string) {
+	jreDir := getJreDir(jreOption)
+	jreLibPath := filepath.Join(jreDir, "lib", "*")
+	self.bootClasspath = newWildcardEntry(jreLibPath)
+	jreExtPath := filepath.Join(jreDir, "lib", "ext", "*")
+	self.extClasspath = newWildcardEntry(jreExtPath)
+}
+
+func (self *Classpath) parseUserClasspath(cpOption string) {
+	if cpOption == "" {
+		cpOption = "."
+	}
+	self.userClasspath = newEntry(cpOption)
+}
+
+func getJreDir(jreOption string) string {
+	if jreOption != "" && exists(jreOption) {
+		return jreOption
+	}
+	if exists("./jre") {
+		return "./jre"
+	}
+	if javaHome := os.Getenv("JAVA_HOME"); javaHome != "" {
+		return filepath.Join(javaHome, "jre")
+	}
+	panic("can not find jre folder!")
+}
+
+func exists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
--- searchclassfile/main.go
+++ searchclassfile/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"JVM-Go/searchclassfile/classpath"
+	"fmt"
+	"strings"
+)
 
 func main() {
 	cmd := parseCmd()
@@ -14,5 +18,13 @@ func main() {
 }
 
 func startJVM(cmd *Cmd) {
-	fmt.Printf("searchclassfile: %s class:%s args:%v\n", cmd.cpOption, cmd.class, cmd.args)
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	fmt.Printf("classpath: %s\nclass: %v\nargs: %v\n", cp, cmd.class, cmd.args)
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		fmt.Printf("Could not find or load main class %s\n", cmd.class)
+		return
+	}
+	fmt.Printf("class data: %v\n", classData)
 }
