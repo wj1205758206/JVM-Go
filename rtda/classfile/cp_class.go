ba958a59d38e34fb5a6ea2d9cf3deb5af1257dee
weijian <1205758206@qq.com>
2021-08-03 22:25:48 +0800

    初步实现线程私有的运行时数据区(线程、PC、JVM栈、栈帧、局部变量表、操作数栈)

---
+++ rtda/classfile/cp_class.go
@@ -0,0 +1,13 @@
+package classfile
+//ConstantClassInfo 结构体定义，用来表示类或者接口的符号引用
+type ConstantClassInfo struct {
+	cp        ConstantPool
+	nameIndex uint16
+}
+
+func (self *ConstantClassInfo) readInfo(reader *ClassReader) {
+	self.nameIndex = reader.readUint16()
+}
+func (self *ConstantClassInfo) Name() string {
+	return self.cp.getUtf8(self.nameIndex)
+}
