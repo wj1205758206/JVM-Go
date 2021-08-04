6e91061cc44a61229ee9f36ff6771a3e4e634b25
weijian <1205758206@qq.com>
2021-08-04 14:55:23 +0800

    定义指令接口结构体

--- instructions/instructions/constants/ipush.go
+++ instructions/instructions/constants/ipush.go
@@ -1,1 +1,27 @@
 package constants
+
+import (
+	"JVM-Go/instructions/instructions/base"
+	"JVM-Go/rtda/rtda"
+)
+
+//BIPUSH
+type BIPUSH struct {
+	val int8
+}
+
+//SIPUSH
+type SIPUSH struct {
+	val int16
+}
+
+//从操作数中获取一个byte型整数，扩展成int型，然后推入栈顶
+func (self *BIPUSH) FetchOperands(reader *base.BytecodeReader) {
+	self.val = reader.ReadInt8()
+}
+
+//从操作数中获取一个short型整数，扩展成int型，然后推入栈顶
+func (self *BIPUSH) Execute(frame *rtda.Frame) {
+	i := int32(self.val)
+	frame.GetOperandStack().PushInt(i)
+}
--- instructions/instructions/constants/nop.go
+++ instructions/instructions/constants/nop.go
@@ -1,1 +1,18 @@
 package constants
+
+import (
+	"JVM-Go/instructions/instructions/base"
+	"JVM-Go/rtda/rtda"
+)
+
+//NOP 空指令不执行任何操作
+type NOP struct {
+	base.NoOperandsInstruction
+}
+
+func (self *NOP) Execute(frame *rtda.Frame) {
+
+}
+
+
+
